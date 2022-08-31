447f74aee894c0d54e0e78653959bc41ffbfd2a2
meraidandouch <meraid@bu.edu>
2022-08-31 12:23:05 -0700

    Update preprocess_expdata.py

--- preprocess_expdata.py
+++ preprocess_expdata.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 file_collection = {}
 file = {}
-file_df = pd.read_csv('/Users/meraidandouch/Desktop/NeuromicsLab/bb_graph_load/load_exp_data/gene_data/GTEx_Analysis_v6_RNA-seq_RNA-SeQCv1.1.8_gene_reads.csv', skiprows = 2, sep='\t')
+file_df = pd.read_csv('pathway/to/data/GTEx_Analysis_v6_RNA-seq_RNA-SeQCv1.1.8_gene_reads.csv', skiprows = 2, sep='\t')
 samples = file_df.columns.values.tolist()
 print(samples)
 
@@ -15,5 +15,5 @@ for i in range(2, len(samples)):
 
 for keys, values in file_collection.items():
     df = file_collection[keys]
-    df.to_csv('/Users/meraidandouch/Desktop/NeuromicsLab/bb_graph_load/load_exp_data/data/'+ keys +".csv",  sep=',', index = False, header = True)
+    df.to_csv('pathway/to/data/'+ keys +".csv",  sep=',', index = False, header = True)
     
